e7bf68d2db356aaf1690a30497181495d4155999
Frikk <frikk.fossan@acando.no>
2018-07-05 21:14:13 +0200

    added nodeimu

--- app.js
+++ app.js
@@ -1,4 +1,44 @@
-const x = 3;
+'use strict';
+//var wia = require('wia')('Your device secret key');
+//var util = require('util')
+var nodeimu  = require('nodeimu');
+var IMU = new nodeimu.IMU();
 
+var tic = new Date();
 
-console.log("X:",3);
+var callback = function (error, data) {
+ var toc = new Date();
+ if (error) {
+   console.log(error);
+   return;
+ }
+ setTimeout(function() { tic = new Date(); IMU.getValue(callback); } , 250 - (toc - tic));
+}
+
+IMU.getValue(callback);
+
+ /*// Send temperature data
+ wia.sensors.publish({
+   name: "temperature",
+   data: data.temperature.toFixed(4) // data received from temperature sensor
+ });
+
+ // Send pressure data
+ wia.sensors.publish({
+   name: "pressure",
+   data: data.pressure.toFixed(4) // data received from pressure sensor
+ });
+
+ // Send humidity data
+ wia.sensors.publish({
+   name: "humidity",
+   data: data.humidity.toFixed(4) // data received from humidity sensor
+ });
+*/
+/*
+//
+wia.stream.on('connect', function() {
+  IMU.getValue(callback);
+});
+
+wia.stream.connect();*/
